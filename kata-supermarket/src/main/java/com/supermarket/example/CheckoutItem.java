0e14cc7429d549b8599fe861e6fc0c2b5860c106
Test developer <test@testmail.com>
2016-12-07 22:30:16 +0530

    GREEN >> All the test casES pAASED

--- kata-supermarket/src/main/java/com/supermarket/example/CheckoutItem.java
+++ kata-supermarket/src/main/java/com/supermarket/example/CheckoutItem.java
@@ -8,41 +8,51 @@ public class CheckoutItem {
 	private Product product;
 
 	public CheckoutItem(String item) {
-	    product = new Product(1, item);
+		product = new Product(1, item);
 		items.add(product);
-		
+
 	}
 
 	public Float totalprice() {
 		Float totalPrice = new Float(0);
 		for (Product item : items) {
-			this.product=item;
-			totalPrice = totalPrice + unitPrice();
+			this.product = item;
+			totalPrice = totalPrice + productPrice(item);
 		}
 		return totalPrice;
 	}
 
-	private Float unitPrice() {
+	private Float productPrice(Product item) {
 		Float totalPrice;
-		switch (this.product.getName()) {
+		int set = 0;
+		int rem = 0;
+		switch (item.getName()) {
 		case "A":
-			totalPrice = new Float(0.50);
+			totalPrice = new Float(totalPrice = new Float(item.getQuanity() * 0.50));
 			break;
 		case "B":
-			totalPrice = new Float(0.30);
+			set = item.getQuanity() / 2;
+			rem = item.getQuanity() % 2;
+			totalPrice = new Float((set * 0.45) + (rem * 0.3));
 			break;
 		case "C":
-			totalPrice = new Float(0.20);
+			totalPrice = new Float(item.getQuanity() * 0.20);
 			break;
 		default:
-			totalPrice = new Float(0.15);
+			totalPrice = new Float(item.getQuanity() * 0.15);
 			break;
 		}
 		return totalPrice;
 	}
 
 	public void addItem(String item, int qty) {
-		items.add(new Product(qty,item));
+		if (product.getName().equals(item)) {
+			product.setQuanity(product.getQuanity() + qty);
+			//items.add(product);
+		} else {
+			items.add(new Product(qty, item));
+		}
+
 	}
 
 }
--- kata-supermarket/src/main/java/com/supermarket/example/Product.java
+++ kata-supermarket/src/main/java/com/supermarket/example/Product.java
@@ -1,20 +1,35 @@
 package com.supermarket.example;
 
 public class Product {
-	private int quanity =0;
-	private String name="";
+	private int quanity = 0;
+	private String name = "";
+	private float basePrice = 0;
+
+	
+	public float getBasePrice() {
+		return basePrice;
+	}
+
+	public void setBasePrice(float basePrice) {
+		this.basePrice = basePrice;
+	}
+
 	public int getQuanity() {
 		return quanity;
 	}
+
 	public void setQuanity(int quanity) {
 		this.quanity = quanity;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Product(int quanity, String name) {
 		super();
 		this.quanity = quanity;
