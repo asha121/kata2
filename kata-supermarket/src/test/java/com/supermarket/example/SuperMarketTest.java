ec112813c98c4a2966cfb6811ad29ccc49ceea74
Test developer <test@testmail.com>
2016-12-07 17:45:12 +0530

    Test - Changes

---
+++ kata-supermarket/src/test/java/com/supermarket/example/SuperMarketTest.java
@@ -0,0 +1,15 @@
+package com.supermarket.example;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class SuperMarketTest {
+
+	@Test
+	public void test_total_price_for_single_item() {
+		fail("Not yet implemented");
+		
+	}
+
+}
