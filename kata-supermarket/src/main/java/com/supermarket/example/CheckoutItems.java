8597de4346af71327bbbf756d8e0e3bda736c365
Test developer <test@testmail.com>
2016-12-07 18:06:41 +0530

    RED >>> test case addes failing for total price

---
+++ kata-supermarket/src/main/java/com/supermarket/example/CheckoutItems.java
@@ -0,0 +1,14 @@
+package com.supermarket.example;
+
+public class CheckoutItems {
+
+	public CheckoutItems() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Float Totalprice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- kata-supermarket/src/test/java/com/supermarket/example/SuperMarketTest.java
+++ kata-supermarket/src/test/java/com/supermarket/example/SuperMarketTest.java
@@ -1,6 +1,6 @@
 package com.supermarket.example;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -8,8 +8,9 @@ public class SuperMarketTest {
 
 	@Test
 	public void test_total_price_for_single_item() {
-		fail("Not yet implemented");
-		
+		CheckoutItems chkOut = new CheckoutItems();
+		assertEquals(new Float(0.50), chkOut.Totalprice());
+
 	}
 
 }
