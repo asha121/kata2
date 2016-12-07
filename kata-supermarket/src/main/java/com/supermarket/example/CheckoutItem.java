295139ca5fc038f9a4e0968665b3e9a8dc0fb1f5
Test developer <test@testmail.com>
2016-12-07 22:43:55 +0530

    REFACTOR >>>> code refactored for two bb item code

--- kata-supermarket/src/main/java/com/supermarket/example/CheckoutItem.java
+++ kata-supermarket/src/main/java/com/supermarket/example/CheckoutItem.java
@@ -24,16 +24,13 @@ public class CheckoutItem {
 
 	private Float productPrice(Product item) {
 		Float totalPrice;
-		int set = 0;
-		int rem = 0;
+		int qty = item.getQuanity();
 		switch (item.getName()) {
 		case "A":
 			totalPrice = new Float(totalPrice = new Float(item.getQuanity() * 0.50));
 			break;
 		case "B":
-			set = item.getQuanity() / 2;
-			rem = item.getQuanity() % 2;
-			totalPrice = new Float((set * 0.45) + (rem * 0.3));
+			totalPrice = new Float(((qty / 2) * 0.45) + ((qty % 2) * 0.3));
 			break;
 		case "C":
 			totalPrice = new Float(item.getQuanity() * 0.20);
@@ -48,7 +45,7 @@ public class CheckoutItem {
 	public void addItem(String item, int qty) {
 		if (product.getName().equals(item)) {
 			product.setQuanity(product.getQuanity() + qty);
-			//items.add(product);
+			// items.add(product);
 		} else {
 			items.add(new Product(qty, item));
 		}
